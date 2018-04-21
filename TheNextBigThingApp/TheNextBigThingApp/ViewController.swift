e2427b3d77b012c310749b981c998f32eac8903e
Ilter Cengiz <iltercengiz@yahoo.com>
2018-04-21 21:06:07 +0400

    Add iOS app project

---
+++ TheNextBigThingApp/TheNextBigThingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheNextBigThingApp
+//
+//  Created by Ilter Cengiz on 21/4/18.
+//  Copyright © 2018 Ilter Cengiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
