d572dc7f72c3c175c08d414bab5c1f7aff9dce0c
Ilter Cengiz <iltercengiz@yahoo.com>
2018-04-21 21:06:24 +0400

    Add framework project

---
+++ AwesomeAPI/AwesomeAPI/AwesomeAPI.h
@@ -0,0 +1,19 @@
+//
+//  AwesomeAPI.h
+//  AwesomeAPI
+//
+//  Created by Ilter Cengiz on 21/4/18.
+//  Copyright © 2018 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AwesomeAPI.
+FOUNDATION_EXPORT double AwesomeAPIVersionNumber;
+
+//! Project version string for AwesomeAPI.
+FOUNDATION_EXPORT const unsigned char AwesomeAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwesomeAPI/PublicHeader.h>
+
+
