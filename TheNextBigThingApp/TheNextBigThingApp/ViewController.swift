f0c940df657c7031a5e25a8d82f15b928e47cf39
Ilter Cengiz <iltercengiz@yahoo.com>
2018-04-27 22:34:51 +0400

    Link and use AwesomeAPI framework in the app

--- TheNextBigThingApp/TheNextBigThingApp/ViewController.swift
+++ TheNextBigThingApp/TheNextBigThingApp/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AwesomeAPI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        AwesomeAPIClient.shared.testRun()
     }
 
     override func didReceiveMemoryWarning() {
