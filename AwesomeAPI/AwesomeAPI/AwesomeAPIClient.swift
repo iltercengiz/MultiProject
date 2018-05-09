927133390a3660f7bd41cf477a6f5b402a353035
Ilter Cengiz <iltercengiz@yahoo.com>
2018-05-09 21:32:30 +0400

    Use Alamofire in AwesomeAPI framework

--- AwesomeAPI/AwesomeAPI/AwesomeAPIClient.swift
+++ AwesomeAPI/AwesomeAPI/AwesomeAPIClient.swift
@@ -7,13 +7,30 @@
 //
 
 import Foundation
+import Alamofire
 
 public final class AwesomeAPIClient {
     
     public static let shared = AwesomeAPIClient()
     
-    public func testRun() {
+    public func testRun(completion: @escaping () -> Void) {
         print("AwesomeAPIClient is up and running! 🎉")
+        
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+            
+            completion()
+        }
     }
     
 }
--- AwesomeAPI/AwesomeAPITests/AwesomeAPITests.swift
+++ AwesomeAPI/AwesomeAPITests/AwesomeAPITests.swift
@@ -21,15 +21,17 @@ class AwesomeAPITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testTestRun() {
+        let e = expectation(description: "Awesome request")
+        
+        AwesomeAPIClient.shared.testRun {
+            e.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10) { error in
+            if error != nil {
+                print("Failed!")
+            }
         }
     }
     
